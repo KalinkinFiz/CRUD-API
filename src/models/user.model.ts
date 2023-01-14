3a5bcff2d85e1ffa704ad5dee204ecf6fc3039bc
KalinkinFiz <kalinkin.fiz@gmail.com>
2023-01-14 17:32:27 +0300

    feat: add deleteUser and updateUser in user.model

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -5,25 +5,62 @@ import { TUser } from './user.type.js';
 class UsersModel {
   private users: TUser[] = [];
 
-  getAllUsers() {
+  getAllUsers(): Promise<TUser[]> {
     return new Promise((resolve) => {
       resolve(this.users);
     });
   }
 
-  getUser(id: string) {
+  getUser(id: string): Promise<TUser | undefined> {
     return new Promise((resolve) => {
-      const findedUser = this.users.find((user) => user.id === id);
-      resolve(findedUser);
+      const foundUser = this.users.find((user) => user.id === id);
+
+      resolve(foundUser);
     });
   }
 
-  addUser(newUser: TUser) {
+  addUser(newUser: TUser): Promise<TUser> {
     return new Promise((resolve) => {
       const id = uuidv4();
       const user = { ...newUser, id };
+
       this.users.push(user);
       resolve(user);
+    });
+  }
+
+  updateUser(user: TUser): Promise<TUser | null> {
+    return new Promise((resolve) => {
+      const userToUpdate = this.users.find(
+        (userInBd) => userInBd.id === user.id,
+      );
+
+      if (!userToUpdate) {
+        resolve(null);
+      } else {
+        const userIdx = this.users.findIndex(
+          (userInBd) => userInBd.id === user.id,
+        );
+
+        this.users[userIdx] = user;
+        resolve(user);
+      }
+    });
+  }
+
+  deleteUser(id: string): Promise<number> {
+    return new Promise((resolve) => {
+      const userIdx = this.users.findIndex((user) => user.id === id);
+
+      if (userIdx === -1) {
+        resolve(404);
+      } else {
+        this.users = [
+          ...this.users.slice(0, userIdx),
+          ...this.users.slice(userIdx + 1),
+        ];
+        resolve(204);
+      }
     });
   }
 }
