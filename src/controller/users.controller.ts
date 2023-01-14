348e153e6d994afdb22420230af4305d262e5051
KalinkinFiz <kalinkin.fiz@gmail.com>
2023-01-14 17:30:59 +0300

    feat: add deleteUser and updateUser

--- src/controller/users.controller.ts
+++ src/controller/users.controller.ts
@@ -2,20 +2,22 @@ import * as http from 'http';
 
 import { userModel } from '../models/user.model.js';
 import { TUser } from '../models/user.type.js';
+import { headers, errorMessages } from '../common/config.js';
+import { response } from '../utils/response.js';
 
 class UsersController {
   usersModel = userModel;
+  headers = headers;
 
-  getUsers = async (
-    _req: http.IncomingMessage,
-    res: http.ServerResponse,
-  ): Promise<void> => {
+  getUsers = async (req: http.IncomingMessage, res: http.ServerResponse) => {
     try {
       const users = await this.usersModel.getAllUsers();
-      res.writeHead(200, { 'Content-Type': 'appliction/json' });
-      res.end(JSON.stringify(users));
+
+      response(req, res, 200, this.headers, users);
     } catch (err) {
-      console.log(err);
+      response(req, res, 500, this.headers, {
+        message: `Error has been occurs during get users`,
+      });
     }
   };
 
@@ -25,16 +27,19 @@ class UsersController {
     id: string,
   ) => {
     try {
-      const user = await this.usersModel.getUser(id);
+      const user = (await this.usersModel.getUser(id)) as TUser;
+
       if (user) {
-        res.writeHead(200, { 'Content-Type': 'appliction/json' });
-        res.end(JSON.stringify(user));
+        response(_req, res, 200, this.headers, user);
       } else {
-        res.writeHead(404, { 'Content-Type': 'appliction/json' });
-        res.end(JSON.stringify({ message: 'User is not found' }));
+        response(_req, res, 404, this.headers, {
+          message: errorMessages.USER_NOT_FOUND,
+        });
       }
     } catch (err) {
-      console.log(err);
+      response(_req, res, 500, this.headers, {
+        message: `Error has been occurs during get user`,
+      });
     }
   };
 
@@ -43,9 +48,63 @@ class UsersController {
     res: http.ServerResponse,
     user: TUser,
   ) => {
-    const newUser = await this.usersModel.addUser(user);
-    res.writeHead(201, { 'Content-Type': 'application/json' });
-    res.end(JSON.stringify(newUser));
+    try {
+      const newUser = (await this.usersModel.addUser(user)) as TUser;
+
+      response(_req, res, 201, this.headers, newUser);
+    } catch (err) {
+      response(_req, res, 500, this.headers, {
+        message: 'Error has been occurs during add new user',
+      });
+    }
+  };
+
+  updateUser = async (
+    req: http.IncomingMessage,
+    res: http.ServerResponse,
+    user: TUser,
+  ) => {
+    try {
+      const updatedUser = (await this.usersModel.updateUser(user)) as TUser;
+
+      if (!updatedUser) {
+        response(req, res, 404, this.headers, {
+          message: errorMessages.USER_NOT_FOUND,
+        });
+      } else {
+        response(req, res, 200, this.headers, updatedUser);
+      }
+    } catch (err) {
+      response(req, res, 500, this.headers, {
+        message: 'Error has been occurs during update user',
+      });
+    }
+  };
+
+  deleteUser = async (
+    req: http.IncomingMessage,
+    res: http.ServerResponse,
+    id: string,
+  ) => {
+    try {
+      const statusCode = await this.usersModel.deleteUser(id);
+
+      if (statusCode === 404) {
+        response(req, res, 404, this.headers, {
+          message: errorMessages.USER_NOT_FOUND,
+        });
+      }
+
+      if (statusCode === 204) {
+        response(req, res, 204, this.headers, {
+          message: errorMessages.USER_DELETED,
+        });
+      }
+    } catch (err) {
+      response(req, res, 500, this.headers, {
+        message: 'Error has been occurs during delete user',
+      });
+    }
   };
 }
 
