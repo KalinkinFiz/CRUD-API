c84fffe464673dbac94efdc92dad06d31a58476c
KalinkinFiz <kalinkin.fiz@gmail.com>
2023-01-14 03:59:12 +0300

    feat: add baseUrl

---
+++ src/common/config.ts
@@ -0,0 +1,14 @@
+// import * as dotenv from 'dotenv';
+// import path from 'path';
+
+// dotenv.config({
+//   path: path.join(__dirname, '../../.env'),
+// });
+
+// const { SERVER_PORT } = process.env;
+
+// export const config = {
+//   PORT: SERVER_PORT ?? 4000,
+// };
+
+export const baseUrl = '/api/users';
