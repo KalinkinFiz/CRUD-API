4304f23a41109bc158c3c05d33d15e3e37402405
KalinkinFiz <kalinkin.fiz@gmail.com>
2023-01-15 17:27:04 +0300

    feat: add enum StatusCode, ValidateMessage

--- src/common/config.ts
+++ src/common/config.ts
@@ -1,24 +1,24 @@
-// import * as dotenv from 'dotenv';
-// import path from 'path';
-
-// dotenv.config({
-//   path: path.join(__dirname, '../../.env'),
-// });
-
-// const { SERVER_PORT } = process.env;
-
-// export const config = {
-//   PORT: SERVER_PORT ?? 4000,
-// };
-
 export const baseUrl = '/api/users';
 
 export const headers = { 'Content-Type': 'application/json' };
 
-export enum errorMessages {
-  uuid = 'Invalid uuid',
-  endpoint = 'Invalid endpoint',
-  body = 'Invalid request body. The request body must contain the following fields: username - a string, age - a number, hobby - an array of strings.',
+export enum StatusCode {
+  OK = 200,
+  CREATED = 201,
+  N0_CONTENT = 204,
+  BAD_REQUEST = 400,
+  NOT_FOUND = 404,
+  INTERNAL_SERVER_ERROR = 500,
+}
+
+export enum ValidateMessage {
+  UUID = 'Invalid uuid',
+  ENDPOINT = 'Invalid endpoint',
+  BODY = 'Invalid request body. The request body must contain the following fields: username - a string, age - a number, hobby - an array of strings',
+}
+
+export enum ErrorMessages {
   USER_NOT_FOUND = 'User not found',
   USER_DELETED = 'The user has been deleted',
+  INTERNAL_SERVER_ERROR = 'Internal server error',
 }
